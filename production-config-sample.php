b3375256b53317518c98517973bc933f0c6adf79
codbear <douglas.meunier@codbear.com>
2019-03-13 18:05:22 +0100

    docs(config): create production and local config sample

---
+++ production-config-sample.php
@@ -0,0 +1,20 @@
+
+19 lines (16 sloc) 862 Bytes
+<?php
+define( 'DB_NAME',          'put your database name here' );
+define( 'DB_USER',          'put your database user here' );
+define( 'DB_PASSWORD',      'put your database password here' );
+define( 'DB_HOST',          'put your database host here' );
+
+ini_set( 'display_errors', Off );
+define( 'WP_DEBUG_DISPLAY', false );
+define( 'WP_DEBUG', false );
+
+define( 'AUTH_KEY',         'put your unique phrase here' );
+define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
+define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
+define( 'NONCE_KEY',        'put your unique phrase here' );
+define( 'AUTH_SALT',        'put your unique phrase here' );
+define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
+define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
+define( 'NONCE_SALT',       'put your unique phrase here' );
